a68ffc406df6c1240b88be56e62c72f32405d4ea
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-07-19 21:06:37 -0500

    completed 2. Starting Out

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ learn-you-a-haskell-for-great-good
 ==================================
 
 My working through of Learn You a Haskell for Great Good!
+
+Completed:
+
+1. Introduction
+- Starting Out
--- baby.hs
+++ baby.hs
@@ -7,3 +7,20 @@ doubleSmallNumber x = if x > 100
                         else x * 2
 
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+-- a comprehension that replaces each odd number greater than 10 with "BANG!" and each odd number that's less than 10 with "BOOM!"
+boomBangs xs = [ if x > 10 then "BANG!" else "BOOM!" | x <- xs, odd x ]
+
+prepositionalPhrases prepositions nouns = [ p ++ " " ++ n | p <- prepositions, n <- nouns ]
+
+length' xs = sum [1 | _ <- xs]
+
+-- a function that takes a string and removes everything except uppercase letters from it
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+
+-- remove all odd numbers without flattening the list
+removeOdds xxs = [ [ x | x <- xs, even x ] | xs <- xxs ]
+
+-- which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24?
+
+triangle24 = [ (a,b,c) | (a,b,c) <- [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2], a + b + c == 24 ]
